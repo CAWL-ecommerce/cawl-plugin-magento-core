440cde156cbd78acc3912f36679959fbf13f0be8
CAWL ecommerce <ecommerce-integrations@cawl.com>
2025-12-18 12:19:37 +0100

    [WLDDEV-376] Fixed decimal quantity amount for CB 3DSecure object

---
+++ Api/Service/CreateRequest/ThreeDSecureQtyCalculatorInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Cawl\PaymentCore\Api\Service\CreateRequest;
+
+use Magento\Quote\Api\Data\CartInterface;
+
+interface ThreeDSecureQtyCalculatorInterface
+{
+    public function calculateNumberOfItems(CartInterface $quote): int;
+}
