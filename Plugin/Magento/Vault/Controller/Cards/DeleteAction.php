fc00a93ae6ec66e207a8ec589b30b94fd4532e00
CAWL ecommerce <ecommerce-integrations@cawl.com>
2026-03-18 08:37:41 +0000

    Release 1.1.30

---
+++ Plugin/Magento/Vault/Controller/Cards/DeleteAction.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace Cawl\PaymentCore\Plugin\Magento\Vault\Controller\Cards;
+
+use Magento\Customer\Model\Session;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Vault\Api\Data\PaymentTokenInterface;
+use Magento\Vault\Controller\Cards\DeleteAction as MagentoDeleteAction;
+use Magento\Vault\Model\PaymentTokenManagement;
+use Cawl\PaymentCore\Api\Service\Token\DeleteTokenServiceInterface;
+
+class DeleteAction
+{
+    private const CAWL_PAYMENT_CODE_PREFIX  = 'worldline';
+
+    /**
+     * @var PaymentTokenManagement
+     */
+    private $paymentTokenManagement;
+
+    /**
+     * @var Session
+     */
+    private $customerSession;
+
+    /**
+     * @var DeleteTokenServiceInterface
+     */
+    private $deleteTokenService;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    public function __construct(
+        PaymentTokenManagement $paymentTokenManagement,
+        Session $customerSession,
+        DeleteTokenServiceInterface $deleteTokenService,
+        StoreManagerInterface $storeManager
+    ) {
+        $this->paymentTokenManagement = $paymentTokenManagement;
+        $this->customerSession = $customerSession;
+        $this->deleteTokenService = $deleteTokenService;
+        $this->storeManager = $storeManager;
+    }
+
+    public function beforeExecute(MagentoDeleteAction $subject): void
+    {
+        $publicHash = $subject->getRequest()->getPostValue(PaymentTokenInterface::PUBLIC_HASH);
+
+        if (!$publicHash) {
+            return;
+        }
+
+        $paymentToken = $this->paymentTokenManagement->getByPublicHash(
+            $publicHash,
+            $this->customerSession->getCustomerId()
+        );
+
+        if (!$paymentToken) {
+            return;
+        }
+
+        if (strpos($paymentToken->getPaymentMethodCode(), self::CAWL_PAYMENT_CODE_PREFIX) !== 0) {
+            return;
+        }
+
+        $gatewayToken = $paymentToken->getGatewayToken();
+        $storeId = (int) $this->storeManager->getStore()->getId();
+
+        $this->deleteTokenService->execute($gatewayToken, $storeId);
+    }
+}
